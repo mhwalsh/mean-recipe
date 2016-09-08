4e624b5de8a7ca1efa7e4ed95ef7c890c036d5bc
Millie Walsh <millie@primeacademy.io>
2016-09-07 20:59:22 -0500

    refactor to use controller as

--- server/public/assests/scripts/client.js
+++ server/public/assests/scripts/client.js
@@ -1,16 +1,21 @@
-var app = angular.module('app', []);
+(function() {
+  var app = angular.module('app', []);
 
-app.controller("RecipeController", ['$scope', '$http', function($scope, $http) {
-  $scope.recipes = [];
+  app.controller("RecipeController", function($http) {
+    var self = this;
+    this.recipes = [];
 
-  var getRecipes = function(){
-      return $http.get('/recipes').then(function(response){
+    var getRecipes = function(){
+      console.log('called getRecipes');
+
+      $http.get('/recipes').then(angular.bind(self, function(response){
         if(response.status !== 200){
           throw new Error('Failed to fetch recipes from the API');
         }
-        $scope.recipes = response.data;
-        return response.data;
-      });
-  };
-  getRecipes();
-}]);
+        this.recipes = response.data;
+      }));
+    };
+    getRecipes();
+
+  });
+}());
